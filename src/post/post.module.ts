eab9ee93947ca560cb9afd89262608f180b3715f
eminentecero <rapael860429@naver.com>
2022-11-08 00:36:14 +0900

    [FEAT] 비디오 업로드 최종 성공

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { Post } from './entities/post.entity';
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Storage } from '@google-cloud/storage';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Post])],
+  controllers: [PostController],
+  providers: [PostService, Storage],
+  exports: [PostService],
+})
 export class PostModule {}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,23 +1,39 @@
 import {
-    BadRequestException,
-    ForbiddenException,
-    HttpStatus,
-    Injectable,
+  BadRequestException,
+  ForbiddenException,
+  HttpStatus,
+  Injectable,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
+import { Bucket, Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class PostService {
-    constructor(
-        @InjectRepository(Post)
-        private postRepository: Repository<Post>,
-    ){
-        this.postRepository = postRepository;
-    }
+  constructor(
+    private storage: Storage,
 
-    async sendPost(video: File): Promise<any>{
-        
-    }
+    @InjectRepository(Post)
+    private postRepository: Repository<Post>,
+  ) {
+    this.postRepository = postRepository;
+    this.storage = new Storage();
+  }
+
+  async sendPost(video: Express.Multer.File): Promise<any> {
+    console.log('들어오니?');
+    const bucket = this.storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+    const filename = Date.now() + '.mp4';
+    // 1. 비디오 클라우드 업로드
+    const blob = bucket.file(filename);
+    const blobStream = blob.createWriteStream();
+
+    blobStream.on('error', (err) => {
+      console.error(err);
+    });
+
+    // 업로드 실행
+    blobStream.end(video.buffer);
+  }
 }
