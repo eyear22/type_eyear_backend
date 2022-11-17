96d069a940356c9717084e3953b675c8bc00b225
eminentecero <rapael860429@naver.com>
2022-11-18 02:04:08 +0900

    [FIX] PR 반영

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -5,12 +5,15 @@ import {
   FileTypeValidator,
   MaxFileSizeValidator,
   ParseFilePipe,
+  HttpStatus,
   Post,
   Req,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostService } from './post.service';
@@ -27,8 +30,12 @@ export class PostController {
     description: '편지를 보낸다.',
   })
   @ApiCreatedResponse({ description: '편지를 보낸다.', type: String })
-  sendPost(@Body() createpostDto: CreatePostDto, @UploadedFile() video:Express.Multer.File){
-    this.postService.sendPost(createpostDto, video);
-    return 'ok';
+  sendPost(
+    @Body() createpostDto: CreatePostDto,
+    @UploadedFile() video: Express.Multer.File,
+    @Res() res: Response,
+  ) {
+    const post = this.postService.sendPost(createpostDto, video);
+    return res.status(HttpStatus.CREATED).json(post);
   }
 }
