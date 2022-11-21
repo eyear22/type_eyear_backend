133467159d266cb542a546068791f762424cbea4
chaerim <qkrdbsk28@naver.com>
2022-11-21 16:37:33 +0900

    feat(auth): jwt-token setting by cookie

--- src/auth/strategies/auth-jwt-refresh.strategy.ts
+++ src/auth/strategies/auth-jwt-refresh.strategy.ts
@@ -1,30 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh',
 ) {
-  constructor() {
+  constructor(private authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_REFRESH_TOKEN_SECRET,
+      ignoreExpiration: true,
       passReqToCallback: true,
+      secretOrKey: process.env.JWT_REFRESH_TOKEN_SECRET,
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const data = request?.cookies['jwt'];
+          if (!data) {
+            return null;
+          }
+          return data.refresh_token;
+        },
+      ]),
     });
   }
 
-  validate(req: Request, payload: any) {
-    const refresh_token = req
-      ?.get('authorization')
-      ?.replace('Bearer', '')
-      .trim();
+  async validate(req: Request, payload: any) {
+    if (!payload) {
+      throw new BadRequestException('invalid jwt token');
+    }
 
-    return {
-      ...payload,
-      refresh_token,
-    };
+    const data = req?.cookies['jwt'];
+    if (!data?.refresh_token) {
+      throw new BadRequestException('invalid refresh token');
+    }
+
+    const user = await this.authService.validRefreshToken(
+      payload.email,
+      data.refresh_token,
+    );
+
+    return user;
   }
 }
