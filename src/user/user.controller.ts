4394f81acdde3efbf2bfadbb92c6ab55ba2fae22
chaerim <qkrdbsk28@naver.com>
2022-11-25 18:05:00 +0900

    fix(swagger): add response dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,13 +8,16 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { json } from 'stream/consumers';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { EmailCheckDto } from './dto/email-check.dto';
 import { UserService } from './user.service';
+import { EamilCheckResponse } from './dto/email-check-response.dto';
+import { CreateUserResponse } from './dto/create-user-response.dto';
+import { PostsResponse } from './dto/posts-response.dto';
 
 @Controller('user')
 @ApiTags('User API')
@@ -26,23 +29,40 @@ export class UserController {
     summary: '유저 생성 API',
     description: '유저를 생성한다.',
   })
-  @ApiCreatedResponse({ description: '유저를 생성한다.', type: json })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'success',
+    type: CreateUserResponse,
+  })
   async getUser(@Body() requestDto: CreateUserDto, @Res() res: Response) {
     const user = await this.userService.createUser(requestDto);
-    return res.status(HttpStatus.CREATED).json(user);
+    const result = {
+      message: 'success',
+      user: user,
+    };
+    return res.status(HttpStatus.CREATED).send(result);
   }
 
   @Get('emailCheck')
   @ApiOperation({
     summary: '이메일 중복 확인 API',
-    description: '이메일 중복 확인',
+    description: '이메일 중복 확인 (false: 사용 불가, true: 사용 가능)',
   })
-  @ApiCreatedResponse({
-    description: '회원가입시 이메일 중복 확인',
-    type: json,
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'success',
+    type: EamilCheckResponse,
   })
-  async emailCheck(@Query() emailCheckDto: EmailCheckDto) {
-    return await this.userService.emailCheck(emailCheckDto);
+  async emailCheck(
+    @Query() emailCheckDto: EmailCheckDto,
+    @Res() res: Response,
+  ) {
+    const isValid = await this.userService.emailCheck(emailCheckDto);
+    const result = {
+      message: 'success',
+      ...isValid,
+    };
+    return res.status(HttpStatus.OK).send(result);
   }
 
   @Get('posts')
@@ -51,12 +71,17 @@ export class UserController {
     summary: '개인 보낸 우편 리스트 API',
     description: '보낸 우편 리스트 확인',
   })
-  @ApiCreatedResponse({
-    description: '개인이 환자에게 보낸 우편 리스트 확인',
-    type: json,
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'success',
+    type: PostsResponse,
   })
   async getPosts(@Res() res: Response) {
     const posts = await this.userService.getPosts();
-    return res.status(HttpStatus.OK).json(posts);
+    const result = {
+      message: 'success',
+      posts: posts,
+    };
+    return res.status(HttpStatus.OK).send(result);
   }
 }
