cd45c5236e94ed8803e55c51fbf0ff47a6061a77
chaerim <qkrdbsk28@naver.com>
2022-12-11 18:38:44 +0900

    feat: add get-hospital-list api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hospital } from 'src/hospital/entities/hospital.entity';
 import { Patient } from 'src/hospital/entities/patient.entity';
 import { Keyword } from 'src/keywords/entities/keyword.entity';
 import { NameWord } from 'src/keywords/entities/nameWord.entity';
@@ -10,7 +11,16 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Post, Keyword, NameWord, Patient])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Post,
+      Keyword,
+      NameWord,
+      Patient,
+      Hospital,
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, KeywordsService],
   exports: [UserService],
