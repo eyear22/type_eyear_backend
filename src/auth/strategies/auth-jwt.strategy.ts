c09fb54bb9bbc99ac9c627cc69905aea4d560f77
chaerim <qkrdbsk28@naver.com>
2022-12-11 15:18:54 +0900

    fix(auth): change login logic to general token

--- src/auth/strategies/auth-jwt.strategy.ts
+++ src/auth/strategies/auth-jwt.strategy.ts
@@ -1,32 +1,18 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => {
-          const data = request?.cookies['jwt'];
-          if (!data) {
-            return null;
-          }
-          return data.access_token;
-        },
-      ]),
-      //true로 설정하면 Passport에 토큰 검증을 위임하지 않고 직접 검증, false는 Passport에 검증 위임
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      //검증 비밀 값(유출 주위)
       secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
   }
 
   async validate(payload: any) {
-    if (payload === null) {
-      throw new UnauthorizedException();
-    }
-    return payload;
+    return { id: payload.id, email: payload.email };
   }
 }
