8bfc3a38ef613a9ebaa8d058a2232d91df0492d7
chaerim <qkrdbsk28@naver.com>
2022-12-25 23:37:37 +0900

    refactor(reservation): create reservation function

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -23,15 +23,9 @@ export class ReservationService {
     createReservationDto: CreateReservationDto,
     userId: number,
   ) {
-    const today = new Date(new Date().getTime() + this.OFFSET);
-
-    if (today >= new Date(createReservationDto.reservationDate)) {
-      throw new ForbiddenException({
-        statusCode: HttpStatus.FORBIDDEN,
-        message: ['오늘 이후 날짜만 예약 가능합니다.'],
-        error: 'Forbidden',
-      });
-    }
+    const { reservationDate, timetableIndex, faceToface } =
+      createReservationDto;
+    this.validateDate(reservationDate);
 
     const user = await this.userRepository.findOne({
       where: {
@@ -43,70 +37,20 @@ export class ReservationService {
       },
     });
 
-    if (user.patient === null || user.hospital === null) {
-      throw new ForbiddenException({
-        statusCode: HttpStatus.FORBIDDEN,
-        message: ['수신인 등록이 되어있지 않습니다.'],
-        error: 'Forbidden',
-      });
-    }
+    await this.isExistReservation(
+      user.hospital.id,
+      reservationDate,
+      timetableIndex,
+    );
 
-    const isExist = await this.reservationRepository
-      .createQueryBuilder('reservation')
-      .where('reservation.hospitalId = :hospitalId', {
-        hospitalId: user.hospital.id,
-      })
-      .andWhere(
-        'date_format(reservation.reservationDate, "%Y-%m-%d") = :reservationDate',
-        { reservationDate: createReservationDto.reservationDate },
-      )
-      .andWhere('reservation.timetableIndex =:timetableIndex', {
-        timetableIndex: createReservationDto.timetableIndex,
-      })
-      .andWhere('reservation.approveCheck =:approve', { approve: 1 })
-      .execute();
+    await this.overlapReservation(user.hospital.id, reservationDate, userId);
 
-    if (isExist.length > 0) {
-      throw new ForbiddenException({
-        statusCode: HttpStatus.FORBIDDEN,
-        message: ['Already the others have reservation'],
-        error: 'Forbidden',
-      });
-    }
-
-    const overlapReservation = await this.reservationRepository
-      .createQueryBuilder('reservation')
-      .where('reservation.hospitalId = :hospitalId', {
-        hospitalId: user.hospital.id,
-      })
-      .andWhere(
-        'date_format(reservation.reservationDate, "%Y-%m-%d") = :reservationDate',
-        { reservationDate: createReservationDto.reservationDate },
-      )
-      .andWhere('reservation.userId =:userId', { userId })
-      .execute();
-
-    if (overlapReservation.length > 0) {
-      throw new ForbiddenException({
-        statusCode: HttpStatus.FORBIDDEN,
-        message: ['Already you have reservation on this day'],
-        error: 'Forbidden',
-      });
-    }
-
-    const result = await this.reservationRepository
-      .createQueryBuilder()
-      .insert()
-      .into(Reservation)
-      .values({
-        reservationDate: () => `'${createReservationDto.reservationDate}'`,
-        timetableIndex: () => `${createReservationDto.timetableIndex}`,
-        faceToface: () => `${createReservationDto.faceToface}`,
-        hospital: () => `'${user.hospital.id}'`,
-        user: () => `'${user.id}'`,
-        patient: () => `'${user.patient.id}'`,
-      })
-      .execute();
+    const result = await this.saveReservation(
+      reservationDate,
+      timetableIndex,
+      faceToface,
+      user,
+    );
 
     return { id: result.identifiers[0].id };
   }
@@ -146,5 +90,101 @@ export class ReservationService {
     const temp2 = temp1.split('-');
 
     return temp2[0].substring(2) + '/' + temp2[1] + '/' + temp2[2];
+  }
+
+  validateDate(data: Date) {
+    const today = new Date(new Date().getTime() + this.OFFSET);
+
+    if (today >= new Date(data)) {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: ['오늘 이후 날짜만 예약 가능합니다.'],
+        error: 'Forbidden',
+      });
+    }
+  }
+
+  async isExistReservation(
+    hospital_id: number,
+    reservationDate: Date,
+    timetableIndex: number,
+  ) {
+    const isExist = await this.reservationRepository
+      .createQueryBuilder('reservation')
+      .where('reservation.hospitalId = :hospital_id', {
+        hospital_id,
+      })
+      .andWhere(
+        'date_format(reservation.reservationDate, "%Y-%m-%d") = :reservationDate',
+        { reservationDate },
+      )
+      .andWhere('reservation.timetableIndex =:timetableIndex', {
+        timetableIndex,
+      })
+      .andWhere('reservation.approveCheck =:approve', { approve: 1 })
+      .execute();
+
+    if (isExist.length > 0) {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: ['Already the others have reservation'],
+        error: 'Forbidden',
+      });
+    }
+  }
+
+  async overlapReservation(
+    hospital_id: number,
+    reservationDate: Date,
+    userId: number,
+  ) {
+    const overlapReservation = await this.reservationRepository
+      .createQueryBuilder('reservation')
+      .where('reservation.hospitalId = :hospital_id', {
+        hospital_id,
+      })
+      .andWhere(
+        'date_format(reservation.reservationDate, "%Y-%m-%d") = :reservationDate',
+        { reservationDate },
+      )
+      .andWhere('reservation.userId =:userId', { userId })
+      .execute();
+
+    if (overlapReservation.length > 0) {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: ['Already you have reservation on this day'],
+        error: 'Forbidden',
+      });
+    }
+  }
+
+  async saveReservation(
+    reservationDate: Date,
+    timetableIndex: number,
+    faceToface: boolean,
+    user: any,
+  ) {
+    if (user.patient === null || user.hospital === null) {
+      throw new ForbiddenException({
+        statusCode: HttpStatus.FORBIDDEN,
+        message: ['수신인 등록이 되어있지 않습니다.'],
+        error: 'Forbidden',
+      });
+    }
+
+    return await this.reservationRepository
+      .createQueryBuilder()
+      .insert()
+      .into(Reservation)
+      .values({
+        reservationDate: () => `'${reservationDate}'`,
+        timetableIndex: () => `${timetableIndex}`,
+        faceToface: () => `${faceToface}`,
+        hospital: () => `'${user.hospital.id}'`,
+        user: () => `'${user.id}'`,
+        patient: () => `'${user.patient.id}'`,
+      })
+      .execute();
   }
 }
